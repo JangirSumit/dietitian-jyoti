9709b76bdb27bf8bf41b6aa5fa9257932feb2bd7
Sumit Jangir <sumitjangir.jecrc@gmail.com>
2021-03-28 10:05:03 +0530

    version 4

--- js/init.js
+++ js/init.js
@@ -1,8 +1,19 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-
+(function ($) {
+  $(function () {
+    $(".sidenav").sidenav();
+    $(".parallax").parallax();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("./sw.js").then(
+      function (registration) {
+        console.log("Service Worker - Registration successful!!!");
+      },
+      function (error) {
+        console.log("Service Worker - Registration Failed", error);
+      }
+    );
+  });
+}
