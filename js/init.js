8d463973488208d48ad311ff619910537d3adceb
Sumit Jangir <sumitjangir.jecrc@gmail.com>
2021-04-10 21:14:26 +0530

    update pwa

--- js/init.js
+++ js/init.js
@@ -18,12 +18,47 @@ if ("serviceWorker" in navigator) {
   });
 }
 
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  setTimeout(() => {
+    $("#install-banner").show(1000);
+  }, 3000);
+
+  document.querySelector("#buttonInstall").addEventListener("click", (e) => {
+    // hide our user interface that shows our A2HS button
+    document.querySelector("#install-banner").style.display = "none";
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the A2HS prompt");
+      } else {
+        console.log("User dismissed the A2HS prompt");
+      }
+      deferredPrompt = null;
+    });
+  });
+
+  document.querySelector("#cancelInstall").addEventListener("click", (e) => {
+    $("#install-banner").hide(1000);
+  });
+});
+
 $(document).ready(function () {
   $(".modal").modal();
   $(".nav-bar").html(getNavBar());
   $(".page-footer").html(getFooter());
   $(".sidenav").sidenav();
   $(".collapsible").collapsible();
+  $("#install-banner").hide();
 
   var payment_id = getParameterByName("payment_id");
 });
---
+++ sw.js
@@ -0,0 +1,67 @@
+const DYNAMIC_CACHE = "dynamic";
+const STATIC_CACHE = "static";
+const CACHE_ITEMS = [
+  "/",
+  "index.html",
+  "about.html",
+  "blogs.html",
+  "cancellation.html",
+  "contacts.html",
+  "faq.html",
+  "payments.html",
+  "plans.html",
+  "terms-conditions.html",
+  "appointment.html",
+  "tips.html",
+  "404.html",
+  "css/materialize.css",
+  "css/materialize.min.css",
+  "css/plans.css",
+  "css/style.css",
+  "css/timeline.css",
+  "js/init.js",
+  "js/materialize.js",
+  "js/materialize.min.js",
+  "manifest.json",
+  "content/logo32.ico",
+  "content/logo64.png",
+  "content/logo128.png",
+  "content/logo256.png",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://code.jquery.com/jquery-2.1.1.min.js",
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(STATIC_CACHE).then(function (cache) {
+      cache.addAll(CACHE_ITEMS);
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  // Perform install steps
+  console.log("Activating Service Worker", event);
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request)
+          .then(function (res) {
+            return caches.open(DYNAMIC_CACHE).then(function (cache) {
+              cache.put(event.request.url, res.clone());
+              return res;
+            });
+          })
+          .catch((error) => {
+            return caches.match("404.html").then((response) => response);
+          });
+      }
+    })
+  );
+});
